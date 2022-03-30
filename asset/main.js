03e41a8df1630abce3a4e09f3470c6a0734d2e8a
dinoarpa98 <dinoarpa98@gmail.com>
2022-03-30 18:02:08 +0200

    end bonus

--- asset/main.js
+++ asset/main.js
@@ -1,8 +1,9 @@
-var app = new Vue({
+var string = new Vue({
 
     el: '#root',
 
     data: {
-        message: 'Ciao Mondo!'
+        message: 'Ciao Mondo!',
+        image: '../asset/offset_comp_772626-opt.jpg',
     }
 })
